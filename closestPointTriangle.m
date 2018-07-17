e7ea665bf78134e8d081650789f5a16f14d095d1
x17wang <40798933+x17wang@users.noreply.github.com>
2018-07-17 11:12:39 +0200

    Add files via upload

---
+++ closestPointTriangle.m
@@ -0,0 +1,64 @@
+function [clp,u,v,w] = closestPointTriangle(p, a, b, c)
+
+ab = b-a;
+ac = c-a;
+ap = p-a;
+d1 = dot(ab,ap);
+d2 = dot(ac,ap);
+if d1 <= 0.0 && d2 <= 0.0
+    u = 1.0;
+    v = 0.0;
+    w = 0.0;
+    clp = a;
+end;
+
+bp = p-b;
+d3 = dot(ab,bp);
+d4 = dot(ac,bp);
+if d3 >= 0.0 && d4 <= d3 
+    u = 0.0;
+    v = 1.0;
+    w = 0.0;
+    clp = b;
+end;
+   
+vc = d1*d4 - d3*d2;
+if vc <= 0.0 && d1 >= 0.0 && d3 <= 0.0 
+    v = d1 / (d1 - d3);
+    u = 1.0 - v;
+    w = 0.0;
+    clp = a + ab * v;
+end;
+
+cp = p - c;
+d5 = dot(ab,cp);
+d6 = dot(ac,cp);
+if d6 >= 0.0 && d5 <= d6
+    u = 0.0;
+    v = 0.0;
+    w = 1.0;
+    clp = c;
+end;
+
+vb = d5*d2 - d1*d6;
+if vb <= 0.0 && d2 >= 0.0 && d6 <= 0.0 
+    w = d2 / (d2 - d6);
+    u = 1.0 - w;
+    v = 0.0;	
+    clp = a + ac * w;
+end;
+
+va = d3*d6 - d5*d4;
+if va <= 0.0 && (d4 - d3) >= 0.0 && (d5 - d6) >= 0.0 
+    w = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+    u = 0.0;
+    v = 1.0 - w;
+    clp = b + (c - b) * w;
+end;
+
+denom = 1.0 / (va + vb + vc);
+v = vb * denom;
+w = vc * denom;
+u = 1.0 - v - w;
+clp = a + ab * v + ac * w;
+
---
+++ createNNLtriangle.m
@@ -0,0 +1,39 @@
+function [NNLt] = createNNLtriangle(V, Fb, sn, nsn, hs, bw, mw)
+
+NNLt = cell(1,nsn);
+mx = max(1,int8(bw/mw));
+head(1,1:mx*mx*mx) = -1;
+list = zeros(1,size(Fb,1));
+for i =1:size(Fb,1)
+    cog = (V(Fb(i,1),:)+V(Fb(i,2),:)+V(Fb(i,3),:))/3.0;
+    xa = int8((cog(1)+0.5*bw)/bw*mx);
+    ya = int8((cog(2)+0.5*bw)/bw*mx);
+    za = int8((cog(3)+0.5*bw)/bw*mx);
+    tmp = mx*mx*za+mx*ya+xa;
+    list(1,i) = head(1,tmp);
+    head(1,tmp) = i;
+end;
+
+for i = 1:nsn
+    pt = sn(i);
+    xa = int8((V(pt,1)+0.5*bw)/bw*mx);
+    ya = int8((V(pt,2)+0.5*bw)/bw*mx);
+    za = int8((V(pt,3)+0.5*bw)/bw*mx);
+    for xi = max(0,xa-1):min(mx-1,xa+1)
+    for yi = max(0,ya-1):min(mx-1,ya+1)
+    for zi = max(0,za-1):min(mx-1,za+1)
+                tri = head(1,mx*mx*zi + mx*yi + xi);
+                while tri~=-1
+                    if pt~=Fb(tri,1)&&pt~=Fb(tri,2)&&pt~=Fb(tri,3)
+                        [clp,u,v,w] = closestPointTriangle(V(pt,:), V(Fb(tri,1),:), V(Fb(tri,2),:), V(Fb(tri,3),:));
+                        if length(clp - V(pt,:)) < hs
+                            NNLt{1,i}=[NNLt{1,i},tri];
+                        end;
+                    end;
+                    tri = list(1,tri);
+                end;
+            end;
+        end;
+    end;
+end;
+          

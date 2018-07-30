51d0dfca0817e31f1edf4dff5423bb550b57f8e7
x17wang <40798933+x17wang@users.noreply.github.com>
2018-07-30 16:20:29 +0200

    Update closestPointTriangle.m

--- closestPointTriangle.m
+++ closestPointTriangle.m
@@ -1,11 +1,11 @@
-function [clp,u,v,w] = closestPointTriangle(p, a, b, c)
+function [clp,u,v,w] = closestPointTriangle(p, a, b, c, u, v, w)
 
 ab = b-a;
 ac = c-a;
 ap = p-a;
 d1 = dot(ab,ap);
 d2 = dot(ac,ap);
-if d1 <= 0.0 && d2 <= 0.0
+if (d1 <= 0.0) && (d2 <= 0.0)
     u = 1.0;
     v = 0.0;
     w = 0.0;
@@ -15,7 +15,7 @@ end;
 bp = p-b;
 d3 = dot(ab,bp);
 d4 = dot(ac,bp);
-if d3 >= 0.0 && d4 <= d3 
+if (d3 >= 0.0) && (d4 <= d3) 
     u = 0.0;
     v = 1.0;
     w = 0.0;
@@ -23,7 +23,7 @@ if d3 >= 0.0 && d4 <= d3
 end;
    
 vc = d1*d4 - d3*d2;
-if vc <= 0.0 && d1 >= 0.0 && d3 <= 0.0 
+if (vc <= 0.0) && (d1 >= 0.0) && (d3 <= 0.0) 
     v = d1 / (d1 - d3);
     u = 1.0 - v;
     w = 0.0;
@@ -33,7 +33,7 @@ end;
 cp = p - c;
 d5 = dot(ab,cp);
 d6 = dot(ac,cp);
-if d6 >= 0.0 && d5 <= d6
+if (d6 >= 0.0) && (d5 <= d6)
     u = 0.0;
     v = 0.0;
     w = 1.0;
@@ -41,7 +41,7 @@ if d6 >= 0.0 && d5 <= d6
 end;
 
 vb = d5*d2 - d1*d6;
-if vb <= 0.0 && d2 >= 0.0 && d6 <= 0.0 
+if (vb <= 0.0) && (d2 >= 0.0) && (d6 <= 0.0) 
     w = d2 / (d2 - d6);
     u = 1.0 - w;
     v = 0.0;	
@@ -49,7 +49,7 @@ if vb <= 0.0 && d2 >= 0.0 && d6 <= 0.0
 end;
 
 va = d3*d6 - d5*d4;
-if va <= 0.0 && (d4 - d3) >= 0.0 && (d5 - d6) >= 0.0 
+if (va <= 0.0) && ((d4 - d3) >= 0.0) && ((d5 - d6) >= 0.0) 
     w = (d4 - d3) / ((d4 - d3) + (d5 - d6));
     u = 0.0;
     v = 1.0 - w;
